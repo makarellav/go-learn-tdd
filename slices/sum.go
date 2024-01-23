dba1281a207f20ef6e79e6cbece9489ce63f83f1
makarellav <makarellads@gmail.com>
2024-01-23 13:28:15 +0200

    add code for 'arrays and slices' section

--- slices/sum.go
+++ slices/sum.go
@@ -1,1 +1,42 @@
 package slices
+
+func Sum(numbers []int) int {
+	sum := 0
+
+	for _, number := range numbers {
+		sum += number
+	}
+
+	return sum
+}
+
+/*
+SumAll calculates the sum for each slice from the arguments and returns the slice with the calculated sums.
+*/
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+/*
+SumAllTails calculates the sum for each slice from the arguments ignoring the first element in the slice and returns the slice with the calculated sums.
+*/
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
--- slices/sum_test.go
+++ slices/sum_test.go
@@ -1,14 +1,95 @@
 package slices
 
-import "testing"
+import (
+	"fmt"
+	"slices"
+	"testing"
+)
+
+func checkSums(t testing.TB, got, want []int) {
+	t.Helper()
+
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("sum some numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(numbers)
+		want := 15
 
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+	t.Run("some an empty slice", func(t *testing.T) {
+		var numbers []int
+
+		got := Sum(numbers)
+		want := 0
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("sum non-empty slices", func(t *testing.T) {
+		got := SumAll([]int{1, 9}, []int{3, 4}, []int{-8, 0})
+		want := []int{10, 7, -8}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("sum empty slices", func(t *testing.T) {
+		got := SumAll([]int{}, []int{}, []int{}, []int{})
+		want := []int{0, 0, 0, 0}
+
+		checkSums(t, got, want)
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("sum tails for non-empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{3, 7, 9}, []int{13, -21, 21, 0}, []int{0, 54, 46, 100, -100})
+		want := []int{16, 0, 100}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("sum tails for empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{}, []int{}, []int{})
+		want := []int{0, 0, 0, 0}
+
+		checkSums(t, got, want)
+	})
+}
+
+func ExampleSum() {
+	numbersToSum := []int{1, 2, 3, 4, 5}
+	sum := Sum(numbersToSum)
+
+	fmt.Println(sum)
+	// Output: 15
+}
+
+func ExampleSumAll() {
+	slicesToSum := [][]int{{1, 2, 3, 4, 5}, {-10, 0, 15}, {100, -99}, {}}
+	allSums := SumAll(slicesToSum...)
+
+	fmt.Println(allSums)
+	// Output: [15 5 1 0]
+}
+
+func ExampleSumAllTails() {
+	slicesToSum := [][]int{{1, 2, 3, 4, 5}, {-10, 0, 15}, {100, -99}, {}}
+	allTailsSums := SumAllTails(slicesToSum...)
+
+	fmt.Println(allTailsSums)
+	// Output: [14 15 -99 0]
 }
